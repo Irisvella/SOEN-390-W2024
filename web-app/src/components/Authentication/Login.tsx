af19746461278d5afac0e840c7dbf9b6b9da4a83
Fatoumatabintabarry <97992954+Fatoumatabintabarry@users.noreply.github.com>
2024-02-04 01:28:29 -0500

    Signup and Login front end
    Used React , Typescript , Material design , Tailwind.css

---
+++ web-app/src/components/Authentication/Login.tsx
@@ -0,0 +1,86 @@
+import { LockOutlined } from "@mui/icons-material";
+import {
+  Container,
+  CssBaseline,
+  Box,
+  Avatar,
+  Typography,
+  TextField,
+  Button,
+  Grid,
+} from "@mui/material";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import FitbitIcon from '@mui/icons-material/Fitbit';
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {};
+
+  return (
+    <>
+      <Container maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            mt: 20,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "salmon" }}>
+            <FitbitIcon />
+          </Avatar>
+          <Typography variant="h5">Login</Typography>
+          <Box sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoFocus
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="password"
+              name="password"
+              label="Password"
+              type="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+
+            <Button
+              fullWidth
+              variant="outlined" 
+              color="warning"
+              sx={{ mt: 3, mb: 2 }}
+              onClick={handleLogin}
+            >
+              Login
+            </Button>
+            <Grid container justifyContent={"flex-end"}>
+              <Grid item>
+                <Link to="/register">Don't have an account? Register</Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </>
+  );
+};
+
+export default Login;
