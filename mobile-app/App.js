1160a3231f4aacac87a590e0b09edbb2a77aa891
Yousef Enein <yousefenein@Yousefs-MacBook-Pro.local>
2024-02-05 22:06:56 -0500

    Clean up

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -2,9 +2,6 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-// import LoginPage from './src/Login/LoginPage.js'; 
-// import  SignUp from './src/Signup/SignUp.js';
-
 import LoginScreen from './src/Screens/LoginScreen';
 import SignupScreen from './src/Screens/SignupScreen';
 
