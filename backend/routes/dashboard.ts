5b12ae0d29c23791021ffe3e1ee63c4ad85c2721
Vaqint <samuelcollette23@hotmail.com>
2024-02-28 15:27:19 -0500

    fixed backend portion of dashboard, changed query to reflect what is needed in front end

--- backend/routes/dashboard.ts
+++ backend/routes/dashboard.ts
@@ -8,6 +8,18 @@ require("dotenv").config();
 
 import { Request, Response, NextFunction } from "express";
 
+interface companyData {
+    id: number;
+    address:string;
+    image_url:string;
+    company_name:string;
+  }
+  interface userData {
+    id: number;
+    address:string;
+    image_url:string;
+    username:string;
+  }
 
   router.get(
     "/",
@@ -24,36 +36,22 @@ import { Request, Response, NextFunction } from "express";
               console.log("decoded ---- ", decoded);
               const { id, role, email } = (<any>decoded).data;
               if (role === "company") {
-                const company = await prisma.$queryRaw`
-                SELECT *
-                FROM property AS p, owned_by AS o
-                WHERE p.id = o.property_id AND ${id} = o.owner_id;
-                `;
+                const company = await prisma.$queryRaw<companyData[]>`SELECT m.id, p.address, p.image_url, m.company_name FROM property AS p, management_companies as m, owned_by as o
+                where m.id = o.owner_id and m.id = ${id} and p.id = property_id;
+                `
+                
                 console.log("a");
-                return res.status(200).json({
-                  companyName: company?.company_name,
-                  address: company?.address,
-                  unitCount: company?.unit_count,
-                  imageUrl: company?.image_url,
-                  parking_count: company?.parking_count,
-                  locker_count: company?.locker_count,
-                  phone: company?.phone_number,
-                });
+                return res.status(200).json(
+                  company
+                );
               } else if (role === "publicUser") {
-                const publicUser = await prisma.$queryRaw`SELECT * FROM property as p, owned_by as o
-                WHERE p.id = o.property_id AND ${id} = o.owner_id;
-                `;
-                  
+                const publicUser = await prisma.$queryRaw<userData[]>`SELECT pu.id, p.address, p.image_url, pu.username FROM property AS p, public_users as pu, owned_by as o
+                where pu.id = o.owner_id and pu.id = ${id} and p.id = property_id;
+                `
                 console.log("b");
-                return res.status(200).json({
-                    companyName: publicUser?.company_name,
-                    address: publicUser?.address,
-                    unitCount: publicUser?.unit_count,
-                    imageUrl: publicUser?.image_url,
-                    parking_count: publicUser?.parking_count,
-                    locker_count: publicUser?.locker_count,
-                    phone: publicUser?.phone_number,
-                });
+                return res.status(200).json(
+                  publicUser
+                );
               }
               console.log("c");
               return res.status(500).json({ message: "Unexpected error" });
