667a109f37ad4724374687be0ce7667140970980
Vaqint <samuelcollette23@hotmail.com>
2024-02-28 17:19:22 -0500

    corrected the login page error, and removed the dependencies from root folder

--- web-app/src/components/Authentication/Login.tsx
+++ web-app/src/components/Authentication/Login.tsx
@@ -18,51 +18,22 @@ import bgImage from '../../bg2.jpg';
 
 const LoginPage = () => {
   const [role, setRole] = useState("");
-  
-
 
   const handleRoleChange = (event: { target: { value: SetStateAction<string>; }; }) => {
-
     setRole(event.target.value);
   };
-  
 
   return (
     <>
-      <Navbar userName={undefined}/> {/* Render the Navbar component */}
-      <div style={{
-        backgroundImage: `url(${bgImage})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundAttachment: 'fixed',
-        height: '100vh',
-        width: '100vw',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
+      <Container maxWidth="xs">
+        <CssBaseline />
         <Box
-
-         sx={{
-  backgroundColor: 'rgba(255, 255, 255, 0.8)',
-  borderRadius: '8px',
-  padding: '0.9rem',
-  boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-  width: '100%',
-  maxWidth: '330px',
-  margin: 'auto',
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  mt: {
-    xs: 5, // Margin top on xs screens
-    sm: 10, // Margin top on sm screens, you can adjust the value as needed
-  },
-  '@media screen and (max-width: 480px)': {
-    maxWidth: '88%', // Override maxWidth for very small screens
-    mt: -18, // Adjust marginTop for very small screens
-  },
-}}
+          sx={{
+            mt: 20,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
         >
           <Avatar sx={{ m: 1, bgcolor: "salmon" }}>
             <FitbitIcon />
@@ -85,11 +56,12 @@ const LoginPage = () => {
               <Link to="/Signup">Don't have an account? Register</Link>
             </Grid>
           </Grid>
-
         </Box>
-      </div>
+      </Container>
     </>
   );
 };
 
 export default LoginPage;
+
+
