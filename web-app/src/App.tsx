884b082cd4adb9742241063bfc8cc22c9597fb0e
sarahabellard <62346787+sarahabellard@users.noreply.github.com>
2024-03-02 17:29:11 -0500

    Protected route

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,36 +1,42 @@
 // App.tsx
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import SignUp from './components/Authentication/SignUp.tsx';
 import Login from './components/Authentication/Login.tsx';
 import MinApp from './components/EditProfile/src/container/MinApp.tsx';
 import DashboardUser from './pages/DashboardUser.tsx';
 import DashboardCompany from './pages/DashboardCompany.tsx';
-import Addemployee from './pages/addemployee.jsx';
 import Employeesinfo from './pages/employeesinfo.jsx';
+import Addemployee from './pages/addemployee.jsx';
 import './App.css';
 import Home from './pages/Home.jsx';
 import ManagementLanding from './pages/ManagementLanding.jsx';
 import CreateListingPage from './pages/CreateListingPage.tsx';
 
 function App() {
+  const isAuthenticated = !!localStorage.getItem('token');
+
   return (
     <Router>
       <Routes>
         <Route path="/Signup" element={<SignUp />} />
-        {/* You can add more routes here */}
-        <Route path="/login" element={<Login />} /> 
-         <Route path="/" element={<Home />} /> 
-         <Route path="/CreateListingPage" element={<CreateListingPage />} /> 
-         <Route path="/ManagementLanding" element={<ManagementLanding />} />
-         <Route path="/ProfileDash" element={<MinApp />} /> 
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        
 
-         <Route path="/dashboard-user" element={<DashboardUser />} />
-         <Route path="/dashboard-company" element={<DashboardCompany />} />
-
-         <Route path="/Employeesinfo" element={<Employeesinfo/>} /> 
-         <Route path="/Addemployee" element={<Addemployee />} /> 
-          
+        {/* Use the PrivateRoute directly for the protected routes */}
+        {isAuthenticated ? (
+          <>
+            <Route path="/CreateListingPage" element={<CreateListingPage />} />
+            <Route path="/ManagementLanding" element={<ManagementLanding />} />
+            <Route path="/ProfileDash" element={<MinApp />} />
+            <Route path="/dashboard-user" element={<DashboardUser />} />
+            <Route path="/dashboard-company" element={<DashboardCompany />} />
+            <Route path="/Employeesinfo" element={<Employeesinfo />} />
+            <Route path="/Addemployee" element={<Addemployee />} />
+          </>
+        ) : null}
       </Routes>
     </Router>
   );
