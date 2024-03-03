2385a9fe7ff4d056c0292a2299075d4c5fb16f9d
Swaggyyyy2 <abisan1998@gmail.com>
2024-03-03 00:28:19 -0500

    Update addEmployee.ts

--- backend/routes/addEmployee.ts
+++ backend/routes/addEmployee.ts
@@ -1,9 +1,10 @@
 import express from "express";
 const router = express.Router();
 import * as z from "zod";
-import bcrypt from "bcryptjs";
 import prisma from "../prisma/client";
-import {Prisma} from "@prisma/client";
+import jwt from "jsonwebtoken";
+import verifyToken from "../middleware/verify-token";
+require("dotenv").config();
 
 import { Request, Response, NextFunction } from "express";
 
@@ -11,76 +12,82 @@ const Employee = z.object({
   username: z.string().min(1),
   companyName: z.string().min(1),
   email: z.string().email(),
+  yearlySalary: z.number().positive(),
   password: z.string().min(6),
   role: z.enum(["manager", "daily_operations", "finance", "other"]),
 });
 
+interface userData {
+  id: number;
+  role: string;
+}
 
 
-router.post(
-    "/",
-    async function (req: Request, res: Response, next: NextFunction) {
-        try {
-            const body = req.body;
-            const result = Employee.safeParse(body);
-            if (!result.success) {
-                console.log("error ---", result.error);
-                console.log("formatted ---- ", result.error.format());
-                return res.status(400).json(result.error.issues);
-            }
-
-            const userExists = await prisma.users.findFirst({
-                where: {
-                  email: body.email,
-                },
+router.get(
+  "/",
+  verifyToken,
+  async function (req: Request, res: Response, next: NextFunction) {
+    try {
+      jwt.verify(
+        req.token as string,
+        process.env.SECRET as jwt.Secret,
+        async (err, decoded) => {
+          if (err) {
+            return res.status(401).json("Unauthorized");
+          } else {
+            console.log("decoded ---- ", decoded);
+            const { id, role } = (<any>decoded).data;
+            if (role === "company") {
+              const body = req.body;
+              const result = Employee.safeParse(body);
+              if (!result.success) {
+                  return res.status(400).json(result.error.issues);
+              }
+  
+              const userExists = await prisma.users.findFirst({
+                  where: {
+                    email: body.email,
+                  },
+                });
+                if (!userExists) {
+                  return res.status(404).json({ message: "User not found" });
+                }
+  
+                async function employExistingUser(email: string) {
+                  await prisma.$transaction(async () => {
+                    const user = await prisma.users.create({
+                      data: {
+                        email: email,
+                      },
+                    });
+  
+              const employeeUser = await prisma.employed_by.create({
+                  data: {
+  
+                      public_user_id: user.id,
+                      company_id: 
+                      yearly_salary: 
+                      role:role,
+                  },
               });
-              if (userExists) {
-                return res.status(409).json({ message: "Email already exists" });
-              }
-
-              async function createEmployeeUser(hashed_password: string) {
-                await prisma.$transaction(async () => {
-                  const user = await prisma.users.create({
-                    data: {
-                      email: body.email,
-                      hashed_password,
-                    },
-                  });
-
-            const employeeUser = await prisma.employed_by.create({
-                data: {
-
-                    public_user_id: user.id,
-                    company_id: user.id,
-                    role: body.role,
-                    yearly_salary: 60000
-                },
-            });
-        });
-    }
-    
-
-            bcrypt.hash(
-                body.password,
-                10,
-                function (err: Error | null, hash: string) {
-                  createEmployeeUser(hash);
-                },
-              );
-
-            return res.status(201).json({ message: "Employee added successfully"});
-        } catch (err) {
-            if (err instanceof z.ZodError){
-                console.log(err.issues);
-                return res.status(400).json({ message: "One or more fields invalid" });
-            }
-            if (err instanceof Prisma.PrismaClientKnownRequestError) {
-                return res.status(409).json({ message: "User exists already" });
-              }
-            return res.status(500).json({ message: "Failed to add employee" });
-        }
-    }
-
+          });
+      }
+              console.log("a");
+              return res.status(200).json({ // employe info needs to be added
+              
+              });
+            } 
+            console.log("b");
+            return res.status(500).json({ message: "Unexpected error" });
+          }
+        },
+      );
+        } 
+    } 
 );
 
 export default router;
+
+                  
+
+  
