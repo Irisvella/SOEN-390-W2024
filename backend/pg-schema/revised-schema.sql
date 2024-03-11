5ce43de9d8e0b19dec1c39f9b35ce82d02fbc6d5
Hisoka <gintama2009@hotmail.ca>
2024-03-11 00:31:56 +0000

    apply db changes

--- backend/pg-schema/revised-schema.sql
+++ backend/pg-schema/revised-schema.sql
@@ -17,64 +17,74 @@ CREATE TABLE public_users (
     profile_image_key TEXT
 );
 
+CREATE TABLE employee_users (
+    user_id INT PRIMARY KEY REFERENCES users(id) ON DELETE CASCADE,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    phone_number TEXT NOT NULL,
+    profile_image_key TEXT
+);
+
 CREATE TABLE management_companies (
     user_id INT PRIMARY KEY REFERENCES users(id) ON DELETE CASCADE,
     company_name TEXT NOT NULL, -- did not add UNIQUE constraint for simplicity
+    address TEXT NOT NULL,
     phone_number TEXT NOT NULL,
     unit_count INT DEFAULT 0 -- there's a trigger for this is all the way at the end
 );
 
-CREATE TABLE company_address (
-    company_id INT PRIMARY KEY REFERENCES management_companies(user_id) ON DELETE CASCADE,
-    country TEXT DEFAULT 'Canada', -- assuming we're doing canada only
-    province TEXT NOT NULL,
-    city TEXT NOT NULL, 
-    street_name TEXT NOT NULL,
-    postal_code CHAR(7) NOT NULL, -- format of 'A3A 3A3'
-    apartment_number TEXT
-);
+-- CREATE TABLE company_address (
+--     company_id INT PRIMARY KEY REFERENCES management_companies(user_id) ON DELETE CASCADE,
+--     country TEXT DEFAULT 'Canada', -- assuming we're doing canada only
+--     province TEXT NOT NULL,
+--     city TEXT NOT NULL, 
+--     street_name TEXT NOT NULL,
+--     postal_code CHAR(7) NOT NULL, -- format of 'A3A 3A3'
+--     apartment_number TEXT
+-- );
 
 CREATE TABLE property (
     id SERIAL PRIMARY KEY,
     company_id INT REFERENCES management_companies(user_id),
     size INT NOT NULL, -- square feet
+    unit_id TEXT NOT NULL, -- sometimes condo unit ids are letters too
+    address TEXT NOT NULL,
     condo_fee NUMERIC(10, 2) NOT NULL,
-    unit_count INT NOT NULL,
-    registration_key uuid DEFAULT gen_random_uuid() UNIQUE
+    UNIQUE (address, unit_id) 
 );
 
-CREATE TABLE property_address (
-    property_id INT PRIMARY KEY REFERENCES property(id) ON DELETE CASCADE,
-    country TEXT DEFAULT 'Canada',
-    province TEXT NOT NULL,
-    city TEXT NOT NULL,
-    street_name TEXT NOT NULL,
-    postal_code CHAR(7) NOT NULL
-);
+-- CREATE TABLE property_address (
+--     property_id INT PRIMARY KEY REFERENCES property(id) ON DELETE CASCADE,
+--     country TEXT DEFAULT 'Canada',
+--     province TEXT NOT NULL,
+--     city TEXT NOT NULL,
+--     street_name TEXT NOT NULL,
+--     postal_code CHAR(7) NOT NULL
+-- );
 
 CREATE TYPE registration_type AS ENUM('renter', 'owner');
 
 CREATE TABLE registration ( -- omitted company_id since it could be found using property_id
     -- this table replaces the previous owned_by table
-    registration_key uuid REFERENCES property(registration_key),
+    registration_key uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     type registration_type NOT NULL,
     issued_at TIMESTAMP DEFAULT NOW(),
     start_date TIMESTAMP DEFAULT NOW(),
     end_date TIMESTAMP, -- null means ongoing renter or owner
     public_user_id INT REFERENCES public_users(user_id) ON DELETE CASCADE,
     property_id INT REFERENCES property(id) ON DELETE CASCADE,
-    PRIMARY KEY (registration_key, issued_at, public_user_id, property_id)
+    UNIQUE (property_id, start_date, end_date) -- only one person at a time can own/rent a specific property
 );
 
 CREATE TYPE employee_role AS ENUM('manager', 'daily operations', 'finance', 'other');
 
 CREATE TABLE employed_by (
-    public_user_id INT REFERENCES public_users(user_id) ON DELETE CASCADE,
+    employee_user_id INT REFERENCES employee_users(user_id) ON DELETE CASCADE,
     company_id INT REFERENCES management_companies(user_id) ON DELETE CASCADE,
     start_date TIMESTAMP DEFAULT NOW(),
     end_date TIMESTAMP DEFAULT NULL, -- null for ongoing employee
     role employee_role NOT NULL,
-    PRIMARY KEY (public_user_id, company_id)
+    PRIMARY KEY (employee_user_id, company_id)
 );
 
 CREATE TABLE amenities (
@@ -102,7 +112,7 @@ CREATE TABLE requests (
     request_priority priority DEFAULT 'low',
     issued_at TIMESTAMP DEFAULT NOW(),
     condo_owner_id INT REFERENCES public_users(user_id),
-    employee_id INT REFERENCES public_users(user_id),
+    employee_id INT REFERENCES employee_users(user_id),
     PRIMARY KEY (condo_owner_id, employee_id, title, issued_at)
 );
 
@@ -135,7 +145,6 @@ EXECUTE FUNCTION increment_unit_count_function();
 
 -- trigger for decrementing unit count of management companies whenever
 -- a property that belongs to that management company is deleted
-
 CREATE FUNCTION decrement_unit_count_function()
 RETURNS TRIGGER AS $$
 BEGIN
