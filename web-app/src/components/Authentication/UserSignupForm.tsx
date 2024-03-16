6658c33291ca146db223409bc1d0f279f70ff90e
Hisoka <gintama2009@hotmail.ca>
2024-03-16 01:51:19 +0000

    change frontend user login form

--- web-app/src/components/Authentication/UserSignupForm.tsx
+++ web-app/src/components/Authentication/UserSignupForm.tsx
@@ -2,9 +2,10 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { Grid, TextField, Button } from "@mui/material";
 
-
 const UserSignupForm = () => {
   const [username, setUsername] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
   const [password, setPassword] = useState("");
@@ -12,151 +13,187 @@ const UserSignupForm = () => {
   const [successMessage, setSuccessMessage] = useState("");
   const [failMessage, setFailMessage] = useState("");
 
-
-
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   const validateEmail = (email: string) => emailRegex.test(email);
   const phoneRegex = /^\d{10}$/; // Adjust based on required format
   const validatePhone = (phone: string) => phoneRegex.test(phone);
   const passwordsMatch = password === confirmPassword;
 
-
-
- 
-  const handleRegister = async (e: { preventDefault: () => void; }) => {
+  const handleRegister = async (e: { preventDefault: () => void }) => {
     e.preventDefault();
     setSuccessMessage("");
     setFailMessage("");
 
-  
     try {
       // Replace 'http://localhost:3000' with actual backend endpoint
-      const response = await fetch('http://localhost:3000/signup/public-user',  {
-        method: 'POST',
+      const response = await fetch("http://localhost:3000/signup/public-user", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify({ username, email, password, phone}), 
+        body: JSON.stringify({
+          firstName,
+          lastName,
+          username,
+          email,
+          password,
+          phoneNumber: phone,
+        }),
       });
-  
+
       const data = await response.json();
-  
+
       if (response.ok) {
-        console.log('Signup successful', data);
-        setSuccessMessage("Signup successful! You can now login.");     
-        window.location.href = '/Login';
+        console.log("Signup successful", data);
+        setSuccessMessage("Signup successful! You can now login.");
+        window.location.href = "/Login";
       } else {
-        console.error('Signup failed:', data.message);
-        setFailMessage(data.message || 'An error occurred. Please try again.');}
+        console.error("Signup failed:", data.message);
+        setFailMessage(data.message || "An error occurred. Please try again.");
+      }
     } catch (error) {
-      console.error('Signup error:', error);
-      setFailMessage('An error occurred. Please try again.');
+      console.error("Signup error:", error);
+      setFailMessage("An error occurred. Please try again.");
     }
   };
 
   return (
     <form onSubmit={handleRegister}>
       {successMessage && (
-        <div style={{ color: 'green', textAlign: 'center', marginBottom: '10px'}}>
+        <div
+          style={{ color: "green", textAlign: "center", marginBottom: "10px" }}
+        >
           {successMessage}
         </div>
       )}
 
       {failMessage && (
-        <div style={{ color: 'red', textAlign: 'center', marginBottom: '10px'}}>
-                {failMessage}
-              </div>
-            )}
+        <div
+          style={{ color: "red", textAlign: "center", marginBottom: "10px" }}
+        >
+          {failMessage}
+        </div>
+      )}
 
-    <Grid container spacing={2}>
-      <Grid item xs={12}>
-        <TextField
-          name="username"
-          required
-          fullWidth
-          id="username"
-          label="Username"
-          autoFocus
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          aria-required
-        />
-      </Grid>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <TextField
+            name="firstName"
+            required
+            fullWidth
+            id="firstName"
+            label="First name"
+            autoFocus
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            aria-required
+          />
+        </Grid>
 
-      <Grid item xs={12}>
-      <TextField
-          required
-          fullWidth
-          id="email"
-          label="Email Address"
-          name="email"
-          error={!validateEmail(email) && email.length > 0}
-          helperText={!validateEmail(email) && email.length > 0 ? "Invalid email format" : ""}
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
+        <Grid item xs={12}>
+          <TextField
+            name="lastName"
+            required
+            fullWidth
+            id="lastName"
+            label="Last name"
+            autoFocus
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            aria-required
+          />
+        </Grid>
 
-      </Grid>
-      <Grid item xs={12}>
-      <TextField
-          required
-          fullWidth
-          id="phone"
-          label="Phone number"
-          name="phone"
-          error={!validatePhone(phone) && phone.length > 0}
-          helperText={!validatePhone(phone) && phone.length > 0 ? "Invalid phone format" : ""}
-          value={phone}
-          onChange={(e) => setPhone(e.target.value)}
-        />
-
-      </Grid>
-      <Grid item xs={12}>
-        <TextField
-          required
-          fullWidth
-          name="password"
-          label="Password"
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+        <Grid item xs={12}>
+          <TextField
+            required
+            fullWidth
+            id="email"
+            label="Email address"
+            name="email"
+            error={!validateEmail(email) && email.length > 0}
+            helperText={
+              !validateEmail(email) && email.length > 0
+                ? "Invalid email format"
+                : ""
+            }
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </Grid>
         <Grid item xs={12}>
-        <TextField
-          required
-          fullWidth
-          name="confirmPassword"
-          label="Confirm Password"
-          type="password"
-          id="confirmPassword"
-          error={!passwordsMatch && confirmPassword.length > 0}
-          helperText={!passwordsMatch && confirmPassword.length > 0 ? "Passwords do not match" : ""}
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-        />
-
+          <TextField
+            required
+            fullWidth
+            id="phone"
+            label="Phone number"
+            name="phone"
+            error={!validatePhone(phone) && phone.length > 0}
+            helperText={
+              !validatePhone(phone) && phone.length > 0
+                ? "Invalid phone format"
+                : ""
+            }
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            required
+            fullWidth
+            name="confirmPassword"
+            label="Confirm Password"
+            type="password"
+            id="confirmPassword"
+            error={!passwordsMatch && confirmPassword.length > 0}
+            helperText={
+              !passwordsMatch && confirmPassword.length > 0
+                ? "Passwords do not match"
+                : ""
+            }
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </Grid>
       </Grid>
-    </Grid>
-  
-    <Button
-          type="submit"
-          fullWidth
-          variant="outlined" 
-          color="warning"
-          sx={{ mt: 3, mb: 2 }}
-          disabled={!validateEmail(email) || !validatePhone(phone) || !passwordsMatch || !email || !phone || !password || !confirmPassword}
-        >
-          Register
-  </Button>
 
-    <Grid container justifyContent="center">
-      <Grid item>
-        <Link to="/login">Already have an account? Login</Link>
+      <Button
+        type="submit"
+        fullWidth
+        variant="outlined"
+        color="warning"
+        sx={{ mt: 3, mb: 2 }}
+        disabled={
+          !validateEmail(email) ||
+          !validatePhone(phone) ||
+          !passwordsMatch ||
+          !email ||
+          !phone ||
+          !password ||
+          !confirmPassword
+        }
+      >
+        Register
+      </Button>
+
+      <Grid container justifyContent="center">
+        <Grid item>
+          <Link to="/login">Already have an account? Login</Link>
+        </Grid>
       </Grid>
-    </Grid>
-
     </form>
   );
 };
