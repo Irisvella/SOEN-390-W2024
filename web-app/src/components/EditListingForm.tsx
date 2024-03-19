b80013484ec686a0f77517716d7dd928f9269eb2
Hisoka <gintama2009@hotmail.ca>
2024-03-19 01:02:14 +0000

    buf fix

--- web-app/src/components/EditListingForm.tsx
+++ web-app/src/components/EditListingForm.tsx
@@ -1,92 +1,94 @@
-import {useState, useEffect} from 'react';
-import { FormControl, FormLabel, Grid, Input, Button, Container } from '@mui/material';
-import { useParams } from 'react-router-dom';
-
-
+import { useState, useEffect } from "react";
+import {
+  FormControl,
+  FormLabel,
+  Grid,
+  Input,
+  Button,
+  Container,
+} from "@mui/material";
+import { json, useParams } from "react-router-dom";
 
 function EditListingForm() {
- const { propertyId } = useParams();  // You need to determine how you're getting this ID
-const [address, setAddress] = useState('');
+  const { propertyId } = useParams(); // You need to determine how you're getting this ID
+  const [address, setAddress] = useState("");
 
-useEffect(() => {
+  useEffect(() => {
     const fetchPropertyDetails = async () => {
-      const token = localStorage.getItem('token');
-    
+      const token = localStorage.getItem("token");
+
       try {
-        const response = await fetch(`http://localhost:3000/createEditListing/?propertyId=${propertyId}`, {//http://localhost:3000/property/${propertyId}
-          method: 'GET',
-          headers: {
-            'Authorization': `Bearer ${token}`,
-            'Content-Type': 'application/json',
-          },
-        });
+        if (!propertyId) return;
+        const response = await fetch(
+          `http://localhost:3000/createEditListing/${propertyId}`,
+          {
+            //http://localhost:3000/property/${propertyId}
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${token}`,
+              "Content-Type": "application/json",
+            },
+          }
+        );
         const data = await response.json();
         console.log(data);
         if (response.ok) {
           setAddress(data.address); // Assuming you're only fetching the address for editing
         } else {
-          console.error('Failed to fetch property details:', data.message);
+          console.error("Failed to fetch property details:", data.message);
         }
       } catch (error) {
-        console.error('Error fetching property details:', error);
+        console.error("Error fetching property details:", error);
       }
     };
-  
+
     fetchPropertyDetails();
-  },
-  
-  
-  [propertyId]); // Adding propertyId as a dependency);
-  
+  }, [propertyId]); // Adding propertyId as a dependency);
 
-const updatePropertyDetails = async () => {
-  const token = localStorage.getItem('token');
-  const payload = {
-    address
-  };
-  console.log('Sending payload:', payload);
-  try {
-    const response = await fetch('http://localhost:3000/createEditListing', { 
-      method: 'PUT', 
-      headers: {
-        'Authorization': `Bearer ${token}`,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(payload),
-    });
+  const updatePropertyDetails = async () => {
+    const token = localStorage.getItem("token");
+    const payload = {
+      address,
+    };
+    console.log("Sending payload:", payload);
+    try {
+      const response = await fetch("http://localhost:3000/createEditListing", {
+        method: "PUT",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      });
 
-    if (!response.ok) {
-      throw new Error('Failed to PropertyDetails');
+      if (!response.ok) {
+        throw new Error("Failed to PropertyDetails");
+      }
+
+      console.log("Profile updated successfully");
+    } catch (error) {
+      console.error("Error updating profile:", error);
     }
-
-    console.log('Profile updated successfully');
-  } catch (error) {
-    console.error('Error updating profile:', error);
-  }
-};
+  };
 
   return (
     <>
-    
-    <Container>
-    <Grid>
-        <FormControl id="address">
-        <FormLabel>Address</FormLabel>
-        <Input
-          type="address"
-          value={address}
-          onChange={(e) => setAddress(e.target.value)}
-        />
-        </FormControl>
-      
-      <Button 
-      onClick={updatePropertyDetails}>Update</Button>
-    </Grid>
-    </Container>
+      <Container>
+        <Grid>
+          <FormControl id="address">
+            <FormLabel>Address</FormLabel>
+            <Input
+              type="address"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+            />
+          </FormControl>
+
+          <Button onClick={updatePropertyDetails}>Update</Button>
+        </Grid>
+      </Container>
     </>
-  )
+  );
 }
 
 export default EditListingForm;
-
-
