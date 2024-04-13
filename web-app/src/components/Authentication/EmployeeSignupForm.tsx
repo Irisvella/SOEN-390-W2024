08f92565a7397436b419b73494623324eba28f8d
Swaggyyyy2 <abisan1998@gmail.com>
2024-04-13 17:04:36 -0400

    Create EmployeeSignupForm.tsx

---
+++ web-app/src/components/Authentication/EmployeeSignupForm.tsx
@@ -0,0 +1,229 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Grid, TextField, Button, MenuItem } from "@mui/material";
+
+const EmployeeSignupForm = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [role, setRole] = useState("");
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+  const [failMessage, setFailMessage] = useState("");
+ 
+  const roles = [
+    { value: "manager", label: "Manager" },
+    { value: "daily_operations", label: "Daily Operations" },
+    { value: "finance", label: "Finance" },
+    { value: "other", label: "Other" },
+  ];
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const validateEmail = (email: string) => emailRegex.test(email);
+  const phoneRegex = /^\d{10}$/; // Adjust based on required format
+  const validatePhone = (phone: string) => phoneRegex.test(phone);
+  const passwordsMatch = password === confirmPassword;
+
+  const handleRegister = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    setSuccessMessage("");
+    setFailMessage("");
+
+    try {
+      // Replace 'http://localhost:3000' with actual backend endpoint
+      const response = await fetch("http://localhost:3000/signup/employee-user", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          firstName,
+          lastName,
+          role,
+          email,
+          password,
+          phoneNumber: phone,
+        }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        console.log("Signup successful", data);
+        setSuccessMessage("Signup successful! You can now login.");
+        window.location.href = "/Login";
+      } else {
+        console.error("Signup failed:", data.message);
+        setFailMessage(data.message || "An error occurred. Please try again.");
+      }
+    } catch (error) {
+      console.error("Signup error:", error);
+      setFailMessage("An error occurred. Please try again.");
+    }
+  };
+
+  return (
+    <form onSubmit={handleRegister} data-testid="employee-signup-form">
+      {successMessage && (
+        <div
+          style={{ color: "green", textAlign: "center", marginBottom: "10px" }}
+        >
+          {successMessage}
+        </div>
+      )}
+
+      {failMessage && (
+        <div
+          style={{ color: "red", textAlign: "center", marginBottom: "10px" }}
+        >
+          {failMessage}
+        </div>
+      )}
+
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <TextField
+            name="firstName"
+            required
+            fullWidth
+            id="firstName"
+            label="First name"
+            autoFocus
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </Grid>
+
+        <Grid item xs={12}>
+          <TextField
+            name="lastName"
+            required
+            fullWidth
+            id="lastName"
+            label="Last name"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </Grid>
+
+        <Grid item xs={12}>
+          <TextField
+            required
+            fullWidth
+            id="email"
+            label="Email address"
+            name="email"
+            error={!validateEmail(email) && email.length > 0}
+            helperText={
+              !validateEmail(email) && email.length > 0
+                ? "Invalid email format"
+                : ""
+            }
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Grid>
+
+        <Grid item xs={12}>
+          <TextField
+            required
+            fullWidth
+            id="phone"
+            label="Phone number"
+            name="phone"
+            error={!validatePhone(phone) && phone.length > 0}
+            helperText={
+              !validatePhone(phone) && phone.length > 0
+                ? "Invalid phone format"
+                : ""
+            }
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          />
+        </Grid>
+
+        <Grid item xs={12}>
+          <TextField
+            required
+            fullWidth
+            id="role"
+            select
+            label="Employee Role"
+            value={role}
+            onChange={(e) => setRole(e.target.value)}
+          >
+            {roles.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+        </Grid>
+
+        <Grid item xs={12}>
+          <TextField
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            value={password}
+            inputProps={{ 'data-testid': 'passwordInput' }}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Grid>
+
+        <Grid item xs={12}>
+          <TextField
+            required
+            fullWidth
+            name="confirmPassword"
+            label="Confirm Password"
+            type="password"
+            id="confirmPassword"
+            error={!passwordsMatch && confirmPassword.length > 0}
+            helperText={
+              !passwordsMatch && confirmPassword.length > 0
+                ? "Passwords do not match"
+                : ""
+            }
+            value={confirmPassword}
+            inputProps={{ 'data-testid': 'confirmPasswordInput' }}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </Grid>
+      </Grid>
+
+      <Button
+        type="submit"
+        fullWidth
+        variant="outlined"
+        color="warning"
+        sx={{ mt: 3, mb: 2 }}
+        disabled={
+         !validateEmail(email) ||
+          !validatePhone(phone) ||
+          !passwordsMatch ||
+          !email ||
+          !phone ||
+          !password ||
+          !confirmPassword ||
+          !role
+        }
+      >
+        Register
+      </Button>
+
+      <Grid container justifyContent="center">
+        <Grid item>
+          <Link to="/login">Already have an account? Login</Link>
+        </Grid>
+      </Grid>
+    </form>
+  );
+};
+
+export default EmployeeSignupForm;
