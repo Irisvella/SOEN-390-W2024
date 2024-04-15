0266274b45d28aa000c19c88630c8d63f8e61dee
Yousef Enein <yousefenein@outlook.com>
2024-04-14 21:22:58 -0400

    Updae fetch link login

--- web-app/src/components/Authentication/CompanyLoginForm.tsx
+++ web-app/src/components/Authentication/CompanyLoginForm.tsx
@@ -14,7 +14,7 @@ const CompanyLoginForm = () => {
   
     try {
       // Replace 'http://localhost:3000' with actual backend endpoint
-      const response = await fetch('https://estate-flow-backend.vercel.app',  {
+      const response = await fetch('https://estate-api-production.up.railway.app/',  {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
