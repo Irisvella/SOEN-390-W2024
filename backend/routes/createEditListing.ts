c11bd98be86ceb272d5434e224f6431f99e2f93f
Vaqint <samuelcollette23@hotmail.com>
2024-05-01 17:20:37 -0400

    fixed units not showing up

--- backend/routes/createEditListing.ts
+++ backend/routes/createEditListing.ts
@@ -201,19 +201,7 @@ router.get("/:propertyId/units", verifyToken, async (req: Request, res: Response
       where: { property_id: parseInt(propertyId) },
       
     });
-    const parking = await prisma.amenities.findFirst({
-      where:{
-          text_id: "Parking",
-          property_id: Number(propertyId),
-      }
-    });
-    const locker = await prisma.amenities.findFirst({
-      where:{
-          text_id: "Locker",
-          property_id: Number(propertyId),
-      }
-    });
-    res.json({units, parking, locker});
+    res.json(units);
     console.log(units);
   } catch (error) {
     console.error('Failed to get units:', error);
